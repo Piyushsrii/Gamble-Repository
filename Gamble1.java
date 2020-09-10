0bc8c40d1efd024de8f12a14ee10984f9a6fe4ad
piyushsrii <piyushnamo888@gmail.com>
2020-09-10 14:15:01 +0530

    [Piyush] Gamble : Generate Random Number to check win or loss

--- Gamble1.java
+++ Gamble1.java
@@ -4,4 +4,9 @@
     {
     static int stakeMoney=100;
     static int betMoney=1;
+
+      public static void main(String[] args) 
+      {
+       int pot = (int)Math.round(Math.random()*1+1);//UserCase2
+      }
     }
