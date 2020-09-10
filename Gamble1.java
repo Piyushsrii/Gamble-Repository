02ad357889daf5dd1f7400f43254fe1c9ff8917b
piyushsrii <piyushnamo888@gmail.com>
2020-09-10 14:22:13 +0530

    [Piyush] Gamble : Condition check in while condition

--- Gamble1.java
+++ Gamble1.java
@@ -7,6 +7,28 @@
 
       public static void main(String[] args) 
       {
-       int pot = (int)Math.round(Math.random()*1+1);//UserCase2
-      }
-    }
+       while(stakeMoney>=p)
+           {
+        
+                 int pot = (int)Math.round(Math.random()*1+1);//UserCase2
+                 
+                     if(pot == 1)
+                       {
+                        wonAmount++;
+                        wonMoney=wonAmount + betMoney;
+                        System.out.println("you win");//UserCase
+                        stakeMoney -= 1;
+                        }
+                      else
+                       {
+                       lossAmount--;
+                       lossMoney=lossAmount-betMoney;
+                       System.out.println("you loss");
+                       stakeMoney -= 1;
+                       }
+
+                       Won_Money += wonMoney;
+                       Loss_Money += lossMoney;
+
+                       }
+                       }
