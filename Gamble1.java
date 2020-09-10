7423bf79beeb816736077439bef0b560b4ffadbe
piyushsrii <piyushnamo888@gmail.com>
2020-09-10 14:29:32 +0530

    [Piyush] Gamble : Add Some New Variable

--- Gamble1.java
+++ Gamble1.java
@@ -5,18 +5,38 @@
     static int stakeMoney=100;
     static int betMoney=1;
 
-      public static void main(String[] args) 
+
+    static int wonMoney=0;
+    static int lossMoney=0;
+    static int total_Won_Money=0;
+    static int total_loss_Money=0;
+    static int Won_Money=0;
+    static int Loss_Money=0;
+    static int wonAmount=0;
+    static int lossAmount=0;
+
+      public static void main(String[] args)
       {
+
+       Scanner scan=new Scanner(System.in);
+       System.out.println("Enter the days");
+       int Num_of_Days=scan.nextInt();
+       System.out.println("Enter the percentage value you want to find");
+       int percentage=scan.nextInt();
+       int p=(stakeMoney*percentage)/100;
+       int i;
+
        while(stakeMoney>=p)
            {
-        
-                 int pot = (int)Math.round(Math.random()*1+1);//UserCase2
-                 
+
+
+              int pot = (int)Math.round(Math.random()*1+1);
+
                      if(pot == 1)
                        {
                         wonAmount++;
                         wonMoney=wonAmount + betMoney;
-                        System.out.println("you win");//UserCase
+                        System.out.println("you win");
                         stakeMoney -= 1;
                         }
                       else
@@ -30,5 +50,4 @@
                        Won_Money += wonMoney;
                        Loss_Money += lossMoney;
 
-                       }
                        }
